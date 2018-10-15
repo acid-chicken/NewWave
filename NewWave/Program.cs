d22d6da1d30bac59acd335e84c2d0795cee8e0f3
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-10-15 21:58:53 +0900

    Create project

---
+++ NewWave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcidChicken.NewWave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
